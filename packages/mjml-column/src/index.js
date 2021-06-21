d36483f6716a01e3cb4967a577dd1163c85553dc
tomsq <finixit@email.cz>
2021-06-21 09:34:19 +0200

    width changed test

--- packages/mjml-column/src/index.js
+++ packages/mjml-column/src/index.js
@@ -195,7 +195,7 @@ export default class MjColumn extends BodyComponent {
           cellpadding: '0',
           cellspacing: '0',
           role: 'presentation',
-          width: '50%',
+          width: '40%',
         })}
       >
         <tbody>
